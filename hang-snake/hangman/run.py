4f04777115dfce3096a1bf687763c9166eba3066
whittendk <n1ckpoddubnyi@gmail.com>
2023-09-21 20:21:45 +0300

    Complete program

--- hang-snake/hangman/run.py
+++ hang-snake/hangman/run.py
@@ -1,33 +1,55 @@
 from common.util import clear_terminal
+import random
+import graphics as graph
+import words
 
 
 def create_secret():
-    return 'capybara'
+    return words.word_list[random.randint(0, len(words.word_list))]
 
 
-SECRET = create_secret()
-n = len(SECRET)
+tries = 6
+word = create_secret()
+word_arr = ['_'] * len(word)
+guessed = []            # guessed letters
+letter = 0
+incorrect = []
+print(*word_arr)
 
-FINAL_FIELD = r'''
-   +----+
-   |    |
-   o    |
-  /|\   |
-  / \   |
-_______/|\_
-'''
-
-# здесь мы наверное хотим иметь исходное поле
-# и понимание, как оно меняется после каждого хода
-FIELD = FINAL_FIELD
-
-while True:
+while tries > 0:
     # make a move!
-    letter = input('Enter your guess: ')
-    if ...:
-        FIELD = ...  # если не угадали, то надо обновить поле
-    else:
-        ...  # мало ли, понадобится...
+    if word_arr.count('_') == 0:
+        print('You won!! (^_^)')
+        break
+    flag = 0
+    while flag == 0:
+        letter = input('Enter your guess:')
+        if letter.isdecimal():
+            print('Incorrect input')
+        elif len(letter) != 1:
+            print('Incorrect input')
+        else:
+            flag = 1
 
-    clear_terminal()
-    print(FIELD)
+    if letter in word and letter not in guessed:   # if letter is correct and have not been guessed yet
+        print('you are right!')
+        guessed.append(letter)
+        for i in range(0, len(word)):
+            if letter == word[i]:
+                word_arr[i] = letter
+    elif letter in word_arr:                       # if letter is already guessed
+        print('you already guessed this letter :/')
+    else:
+        print('incorrect letter :(')               # if letter is incorrect
+        incorrect.append(letter)
+        tries -= 1
+
+
+    print(graph.img[-tries+6])
+    print(*word_arr)
+    print('incorrect letters:', *incorrect)
+else:
+    print('You lost :( Try again')
+
+print('The word was: ' + word)
+
